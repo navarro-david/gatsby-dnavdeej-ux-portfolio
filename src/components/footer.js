d03f71a753dd4a5a93fe46e6efee7d4bd595ecfe
David Navarro <navarro.david.j@gmail.com>
2020-12-11 09:09:27 -0800

    Footer description update

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { BiCoffee } from "react-icons/bi";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Coffee hits different <span className="icon"><BiCoffee/></span></p>
+      <p>Site, powered by Gatsby. Designer, powered by coffee. <span className="icon"><BiCoffee/></span></p>
     </div>
   </footer>
 )
