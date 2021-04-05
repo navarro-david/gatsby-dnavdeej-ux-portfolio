c0994c47ed6f228ab2ad636cc7aec545d8a1b186
David Navarro <navarro.david.j@gmail.com>
2021-04-05 12:21:11 -0700

    Adding code to be used after react-spring pushes out a new release

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -1,28 +1,40 @@
 import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
+import { useSpring, animated } from 'react-spring'
 
-const PostCard = ({ data }) => (
-  <article className="post-card">
-    {data.frontmatter.featuredImage ? 
-      (
-        <Link to={data.frontmatter.slug}>
-          <Img 
-            fluid={data.frontmatter.featuredImage.childImageSharp.fluid} 
-            objectFit="cover"
-            objectPosition="50% 50%"
-            alt={data.frontmatter.title + ' - Featured image'}
-            className="featured-image"
-          />
-        </Link>
-      ) : ""
-    }
-    <div class="post-content">
-      <h2 className="title"><Link to={data.frontmatter.slug}>{data.frontmatter.title}</Link></h2>
-      <p className="preview"><time>{data.frontmatter.preview}</time></p>
-      <p className="meta"><time>{data.frontmatter.date}</time></p>
-    </div>
-  </article>
-)
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+const PostCard = ({ data }) => {
+  const [props, spring] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+  return (
+    <article className="post-card"
+      // Leaving this code here hoping that a upcoming fix will fix these problems
+      // onMouseMove={({ clientX: x, clientY: y }) => spring.update({ xys: calc(x, y) })}
+      // onMouseLeave={() => spring.update({ xys: [0, 0, 1] })}
+      // style={{ transform: props.xys.to(trans) }}
+    >
+      {data.frontmatter.featuredImage ?
+        (
+          <Link to={data.frontmatter.slug}>
+            <Img
+              fluid={data.frontmatter.featuredImage.childImageSharp.fluid}
+              objectFit="cover"
+              objectPosition="50% 50%"
+              alt={data.frontmatter.title + ' - Featured image'}
+              className="featured-image"
+            />
+          </Link>
+        ) : ""
+      }
+      <div class="post-content">
+        <h2 className="title"><Link to={data.frontmatter.slug}>{data.frontmatter.title}</Link></h2>
+        <p className="preview"><time>{data.frontmatter.preview}</time></p>
+        <p className="meta"><time>{data.frontmatter.date}</time></p>
+      </div>
+    </article>
+  )
+}
 
 export default PostCard
