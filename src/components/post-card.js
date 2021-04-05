b5596e20fd5faa2c514e09308488797ef500a4d4
David Navarro <navarro.david.j@gmail.com>
2021-04-05 16:09:54 -0700

    Attempting to fix animation, will probably scrap. Saving just in case.

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -1,19 +1,40 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 import { useSpring, animated } from 'react-spring'
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
 
 const PostCard = ({ data }) => {
-  const [props, spring] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+
+  const [height, setHeight] = useState(0)
+  const [width, setWidth] = useState(0)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight)
+    setHeight(ref.current.clientWidth)
+  })
+
+
+  const [props, api] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 1, tension: 350, friction: 40 } }))
+  const calc = (x, y) => [-(y - height / 2) / 100, (x - width / 2) / 100, 1]
+  const trans = (x, y, s) => `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+
   return (
-    <article className="post-card"
-      // Leaving this code here hoping that a upcoming fix will fix these problems
-      // onMouseMove={({ clientX: x, clientY: y }) => spring.update({ xys: calc(x, y) })}
-      // onMouseLeave={() => spring.update({ xys: [0, 0, 1] })}
-      // style={{ transform: props.xys.to(trans) }}
+    <animated.div 
+      className="post-card"
+      onMouseMove={({ clientX: x, clientY: y }) => {
+        // console.log('Mouse moved') 
+        api.set({ xys: calc(x, y) })
+      }}
+      onMouseLeave={() => {
+        // console.log('Mouse left')
+        api.set({ xys: [0, 0, 1] })
+      }}
+      style={{ transform: props.xys.to(trans) }}
+      ref={ref}
     >
       {data.frontmatter.featuredImage ?
         (
@@ -33,7 +54,7 @@ const PostCard = ({ data }) => {
         <p className="preview"><time>{data.frontmatter.preview}</time></p>
         <p className="meta"><time>{data.frontmatter.date}</time></p>
       </div>
-    </article>
+    </animated.div>
   )
 }
 
